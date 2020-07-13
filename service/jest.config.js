0709fb2a12bfac947de03e3f9b2faf10640dc046
Michael Hargiss <mdhargiss@gmail.com>
2020-07-13 12:58:00 -0400

    resolve modules in jest

--- service/jest.config.js
+++ service/jest.config.js
@@ -1,6 +1,13 @@
 // https://github.com/facebook/jest/blob/master/docs/Configuration.md
 
+const path = require('path')
+
 module.exports = {
+  moduleDirectories: [
+    'node_modules',
+    path.join(__dirname, 'src'),
+    path.join(__dirname, 'test'),
+  ],
   testEnvironment: 'jest-environment-node',
   verbose: true,
 }
