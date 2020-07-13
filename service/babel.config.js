8c4211ea9fa4144e1bb75c0828748cab67775f02
Michael Hargiss <mdhargiss@gmail.com>
2020-07-13 12:58:54 -0400

    compile for test only (will use webpack for prod)

--- service/babel.config.js
+++ service/babel.config.js
@@ -1,8 +1,11 @@
+const isTest = String(process.env.NODE_ENV) === 'test'
+
 module.exports = {
   presets: [
     [
       '@babel/preset-env',
       {
+        modules: isTest ? 'commonjs' : false,
         targets: {
           node: 'current',
         },
